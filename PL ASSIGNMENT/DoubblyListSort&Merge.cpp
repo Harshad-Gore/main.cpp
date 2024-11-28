e11b411e52799423e3ed16e2a5b790a358a6dc3b
Harshad-Gore <202301040138@mitaoe.ac.in>
2024-11-28 11:06:26 +0530

    refactor doubly linked list implementation: improve memory management, enhance bubble sort, and update merge function

--- PL ASSIGNMENT/DoubblyListSort&Merge.cpp
+++ PL ASSIGNMENT/DoubblyListSort&Merge.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 using namespace std;
+
 class node
 {
 public:
@@ -7,27 +8,41 @@ public:
     int marks;
     node *next;
     node *prev;
-    node()
-    {
+
+    node(){
         next = prev = NULL;
     }
 };
+
 class marklist
 {
 public:
     node *head;
-    marklist()
-    {
-        head = NULL;
+
+    marklist()  {
+    head =NULL;
     }
-    int create();
+
+    ~marklist()
+    {
+        node *temp;
+        while (head)
+        {
+            temp = head;
+            head = head->next;
+            delete temp;
+        }
+    }
+
+    void create();
     void print();
-    void bubbleSort(int n);
+    void bubbleSort();
     int countNodes();
     node *swap(node *ptr1, node *ptr2);
-    marklist merge(marklist l1, marklist l2);
+    marklist merge(marklist &l1, marklist &l2);
 };
-int marklist::create()
+
+void marklist::create()
 {
     node *temp;
     char ch;
@@ -38,88 +53,34 @@ int marklist::create()
         cin >> temp->rollNo;
         cout << "Enter marks:" << endl;
         cin >> temp->marks;
+
         if (head == NULL)
+        {
             head = temp;
+        }
         else
         {
             temp->next = head;
             head->prev = temp;
             head = temp;
         }
-        cout << "do you want to add more nodes(Y/n)" << endl;
+
+        cout << "Do you want to add more nodes (Y/n): ";
         cin >> ch;
     } while (ch == 'y' || ch == 'Y');
 }
+
 void marklist::print()
 {
     node *temp = head;
     while (temp != NULL)
     {
-        cout << "Roll No:" << temp->rollNo << endl;
-        cout << "Marks:" << temp->marks << endl;
+        cout << "Roll No: " << temp->rollNo << endl;
+        cout << "Marks: " << temp->marks << endl;
         temp = temp->next;
     }
 }
-node *marklist::swap(node *p1, node *p2)
-{
-    if (p1->prev == NULL)
-    {
-        node *temp = p2->next;
-        p2->next = p1;
-        p2->prev = NULL;
-        p1->prev = p2;
-        p1->next = temp;
-        temp->prev = p1;
-        return p2;
-    }
-    else if (p2->next == NULL)
-    {
-        node *temp = p1->prev;
-        p2->next = p1;
-        p1->next = NULL;
-        p2->prev = p2;
-        p1->prev = temp;
-        temp->next = p2;
-        return p2;
-    }
-    else if (p2->next == NULL)
-    {
-        node *temp1 = p1->prev;
-        node *temp2 = p2->next;
-        p2->next = p1;
-        p1->prev = p2;
-        p2->prev = temp1;
-        temp1->next = p2;
-        p1->next = temp2;
-        temp2->prev = p1;
-        return p2;
-    }
-}
-void marklist::bubbleSort(int cnt)
-{
-    node **h;
-    int swapped;
-    for (int i = 0; i < cnt; i++)
-    {
-        h = &(head);
-        swapped = 0;
-        for(int j =0; j < cnt - i - 1; j++)
-        {
-            node *p1 = *h;
-            node *p2 = p1->next;
-            if(p1->marks>p2->marks)
-            {
-                *h = swap(p1,p2);
-                swapped = 1;
-            }
-            h = &(*h)->next;
-            if (swapped == 0)
-            {
-                break;
-            }
-        }
-    }
-}
+
 int marklist::countNodes()
 {
     int count = 0;
@@ -131,14 +92,62 @@ int marklist::countNodes()
     }
     return count;
 }
-marklist marklist::merge(marklist l1, marklist l2)
+
+node *marklist::swap(node *p1, node *p2)
+{
+    if (p2 == NULL) return p1;
+
+    if (p1->prev)
+        p1->prev->next = p2;
+    if (p2->next)
+        p2->next->prev = p1;
+
+    p2->prev = p1->prev;
+    p1->next = p2->next;
+    p2->next = p1;
+    p1->prev = p2;
+
+    return p2;
+}
+
+void marklist::bubbleSort()
+{
+    int n = countNodes();
+    if (n <= 1)
+        return;
+
+    for (int i = 0; i < n - 1; ++i)
+    {
+        node *current = head;
+        bool swapped = false;
+
+        while (current && current->next)
+        {
+            if (current->marks > current->next->marks)
+            {
+                node *nextNode = current->next;
+                head = (current == head) ? swap(current, nextNode) : head;
+                swapped = true;
+            }
+            else
+            {
+                current = current->next;
+            }
+        }
+
+        if (!swapped)
+            break;
+    }
+}
+
+marklist marklist::merge(marklist &l1, marklist &l2)
 {
     marklist l3;
     node *p = l1.head;
     node *q = l2.head;
-    node *r = nullptr;
+    node *r = NULL;
 
-    while (p != nullptr && q != nullptr)
+    while (p != NULL && q != NULL)
     {
         node *temp = new node;
         if (p->marks < q->marks)
@@ -154,7 +163,7 @@ marklist marklist::merge(marklist l1, marklist l2)
             q = q->next;
         }
 
-        if (l3.head == nullptr)
+        if (l3.head == NULL)
         {
             l3.head = temp;
             r = l3.head;
@@ -167,7 +176,7 @@ marklist marklist::merge(marklist l1, marklist l2)
         }
     }
 
-    while (p != nullptr)
+    while (p != NULL)
     {
         node *temp = new node;
         temp->rollNo = p->rollNo;
@@ -178,7 +187,7 @@ marklist marklist::merge(marklist l1, marklist l2)
         p = p->next;
     }
 
-    while (q != nullptr)
+    while (q != NULL)
     {
         node *temp = new node;
         temp->rollNo = q->rollNo;
@@ -194,10 +203,14 @@ marklist marklist::merge(marklist l1, marklist l2)
 
 int main()
 {
-    marklist m1,l1,l2,l3;
+    marklist m1;
     m1.create();
-    int cnt = m1.countNodes();
-    m1.bubbleSort(cnt);
+    cout << "\nOriginal List:\n";
     m1.print();
-    return 0;   
+
+    m1.bubbleSort();
+    cout << "\nSorted List:\n";
+    m1.print();
+
+    return 0;
 }
